422766de54029a3604e141a34a10e1cb0c491cfe
Pete Sekesn <72456128+The-Spruce-Goose@users.noreply.github.com>
2020-11-14 18:20:03 -0500

    script overhaul

--- script.js
+++ script.js
@@ -78,34 +78,110 @@ const STORE = {
   /********** TEMPLATE GENERATION FUNCTIONS **********/
   
   // These functions return HTML templates
-  function generateIntro(){
-    $('main').append('<h1 class= startScreen>Disney Quiz App</h1>');
-  $('main').append('<p class= startScreen> Welcome to my Disney Quiz App. Please click the button below to begin</p>');
-  $('main').append('<button class= btn> Let the Magic Begin </button>')
-  console.log('generateIntro ran');
+  $( document ).ready(function() { 
+    generateIntroScreen()
+    console.log( "Doc is ready!" ); 
+  });
+  function generateIntroScreen(){
+    let appDiv = $('<div>')
+    appDiv.attr('id', 'app');
+    let welcomeDiv = $('<div>')
+    welcomeDiv.addClass('welcome');
+    let header = $('<h1>')
+    header.html('Disney Parks Quiz');
+    welcomeDiv.append(header);
+    
+    let welcomeForm = $('<form>');
+    let welcomeText = $('<p>');
+    welcomeText.html(' Welcome to the Disney Quiz App');
+
+    let startButton = $('<button>');
+    startButton.attr('type', 'submit');
+    startButton.attr('id', 'startQuiz');
+    startButton.html('start');
+    startButton.click({screenNumber:1, score:0},renderNext);
+
+    welcomeForm.append(welcomeText);
+    welcomeForm.append(startButton);
+
+    welcomeDiv.append(welcomeForm);
+    appDiv.append(welcomeDiv);
+$('body').append(appDiv);
   }
 
+  /*return
+    <div class="welcome">
+      <h1>Disney Parks Quiz</h1>
+    <form>
+      <p>
+        Welcome to my Disney Quiz! Test your Disney Parks & Resorts trivia knowledge.
+      </p>
+      
+      <button type="submit"id="startQuiz" autofocus>Let The Magic Begin!</button>
+    </form>
+  </div>
+  */
+    
 
-  function generateAnswersList(answers){
 
-  }
-
+  
 
   
   /********** RENDER FUNCTION(S) **********/ 
-
-  function renderQuestionText(){
-    
-    
-    console.log('renderQuestionText ran');
+function renderNext(event){
+  $('#app').empty()
+  let questionDiv = $('<div>');
+ let questionHeader = $('<h2>');
+ questionHeader.html('Score : ' + event.data.score);
+ questionDiv.append(questionHeader);
+  if (event.data.screenNumber == 1){
+    let submitButton = $('<button>');
+    submitButton.attr('type', 'submit');
+    submitButton.attr('id', 'nextQuestion');
+    submitButton.html('Submit');
+    submitButton.click({screenNumber:2, score:5},renderNext);
+questionDiv.append(submitButton);
   }
+  else if(event.data.screenNumber == 2){
+    let submitButton = $('<button>');
+    submitButton.attr('type', 'submit');
+    submitButton.attr('id', 'nextQuestion');
+    submitButton.html('Submit');
+    submitButton.click({screenNumber:3, score:5},renderNext);
+questionDiv.append(submitButton);
+
+  }
+  else if(event.data.screenNumber == 3){
+    let submitButton = $('<button>');
+    submitButton.attr('type', 'submit');
+    submitButton.attr('id', 'nextQuestion');
+    submitButton.html('Submit');
+    submitButton.click({screenNumber:4, score:5},renderNext);
+questionDiv.append(submitButton);
+
+  }
+  else if(event.data.screenNumber == 4){
+    let submitButton = $('<button>');
+    submitButton.attr('type', 'submit');
+    submitButton.attr('id', 'nextQuestion');
+    submitButton.html('Submit');
+    submitButton.click({screenNumber:5, score:5},renderNext);
+questionDiv.append(submitButton);
+  }
+  else if(event.data.screenNumber == 5){
+    let finishButton = $('<button>');
+    finishButton.attr('type', 'submit');
+    finishButton.attr('id', 'nextQuestion');
+    finishButton.html('Submit');
+    finishButton.click({screenNumber:5, score:5},renderNext);
+questionDiv.append(finishButton)
+  }
+  $('#app').append(questionDiv);
+}
+
   // This function conditionally replaces the contents of the <main> tag based on the state of the store
   
+
   /********** EVENT HANDLER FUNCTIONS **********/
   
   // These functions handle events (submit, click, etc)
-  function handleAnswerSubmitted(){
-
-  }
-
-$(generateIntro)
